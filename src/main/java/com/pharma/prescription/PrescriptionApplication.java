f8111ddd79764d9e81f14a32ad22bc81822d8718
Yonas Abraha <yonininu@gmail.com>
2024-03-20 21:35:52 -0400

    Intial commit

---
+++ src/main/java/com/pharma/prescription/PrescriptionApplication.java
@@ -0,0 +1,14 @@
+package com.pharma.prescription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrescriptionApplication.class, args);
+
+    }
+
+}
